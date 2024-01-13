2efb76cf2b65bc650c3c57d316ca169157b75212
Ibrahim Haseeb <74272079+cloudyonthemic@users.noreply.github.com>
2024-01-13 17:11:48 +0300

    Fixed color change

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
 from pygovee import Govee
 
 # Replace the following line with your Govee API key
-api_key = "API_KEY"
+api_key = "c25dd77d-3872-431e-bc40-dca439ca88f7"
 client = Govee.GoveeClient(apiKey=api_key)
 client.login()
+
+
 
 inp = input(
     "Welcome to Govee settings. \n 1. Turn on \n 2. Turn off \n 3. Change color \n 4. Change brightness \n"
 )
 
 if inp == "1":
-    client.device_on("MAC_ADDRESS", "MODEL")
+    client.device_on("9E:53:A4:C1:38:D5:2C:14", "H6154")
 elif inp == "2":
-    client.device_off("MAC_ADDRESS", "MODEL")
+    client.device_off("9E:53:A4:C1:38:D5:2C:14", "H6154")
 elif inp == "3":
     color = input(
         "Enter color: \n 1. Red \n 2. Green \n 3. Blue \n 4. Yellow \n 5. Purple \n 6. Cyan \n 7. White \n 8. Orange \n 9. Pink \n 10. Custom \n"
     )
     if color == "1":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "red")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 255,0,0)
     elif color == "2":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "green")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 0,255,0)
     elif color == "3":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "blue")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 0,0,255)
     elif color == "4":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "yellow")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 255,255,0)
     elif color == "5":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "purple")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 255,0,255)
     elif color == "6":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "cyan")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 0,255,255)
     elif color == "7":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "white")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 255,255,255)
     elif color == "8":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "orange")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 255,165,0)
     elif color == "9":
-        client.device_set_color("MAC_ADDRESS", "MODEL", "pink")
+        client.change_device_color("9E:53:A4:C1:38:D5:2C:14", "H6154", 255,192,203)
     elif color == "10":
-        client.device_set_color(
-            "MAC_ADDRESS", "MODEL", input("Enter color: ")
+        client.change_device_color(
+            "9E:53:A4:C1:38:D5:2C:14", "H6154", input("Enter color: ")
         )
